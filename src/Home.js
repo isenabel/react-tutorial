73d81321c104419caaca39bb67568749b48340fa
Isen Abel Liu Then <isenabel@hotmail.com>
2024-05-13 16:30:09 -0400

    fix

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,7 @@ import useFetch from "./hooks/useFetch";
 
 const Home = () => {
 
-  // const { data: blogs, isPending, error } = useFetch('https://3.142.12.117:80/blogs', 10);
-  const { data: blogs, isPending, error } = useFetch('https://localhost:80/blogs', 10);
+  const { data: blogs, isPending, error } = useFetch('https://3.142.12.117:80/blogs', 10);
 
   return (
     <div className="home">
